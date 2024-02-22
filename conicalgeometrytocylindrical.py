4ecd47502017ddd0126054890069c70907c9c15c
c-m-17 <160599737+c-m-17@users.noreply.github.com>
2024-02-22 15:27:46 +0000

    v2.0
    completed function file for individual strake conical geometry conversion to cylindrical

--- conicalgeometrytocylindrical.py
+++ conicalgeometrytocylindrical.py
@@ -16,18 +16,13 @@ def inclinedMeridionalLength(h,beta):
 # returns the *cylindrical* geometry of a given strake
 def findStrakeGeometry(filename,strakeID):
     geoms = pd.read_csv(filename);
+
     h = geoms['h (mm)'].loc[geoms["ID"] == strakeID];
     beta = geoms['beta (rad)'].loc[geoms["ID"] == strakeID];
     d_1 = geoms['d1 (top) (mm)'].loc[geoms["ID"] == strakeID];
     d_2 = geoms['d2 (bottom) (mm)'].loc[geoms["ID"] == strakeID];
+    t = geoms['t (mm)'].loc[geoms["ID"] == strakeID];
     
     l = inclinedMeridionalLength(h, beta);
     R = averageRadius(d_1, d_2, beta);
-    t = geoms['t (mm)'].loc[geoms["ID"] == strakeID];
-    return [l[1],R[1],t[1]]
-
-# import from csv file
-filename = "Sadowskietal2023-benchmarkgeometries.csv"
-
-# gives list of [H,R,t] values for given strake, in mm
-strakeGeometry = findStrakeGeometry(filename, 102);
+    return [l.unique(),R.unique(),t.unique()]
