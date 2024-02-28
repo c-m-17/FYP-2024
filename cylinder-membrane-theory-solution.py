6eb9785e46ea5f03b277fa9931987b5ffbd0a047
c-m-17 <160599737+c-m-17@users.noreply.github.com>
2024-02-28 10:25:00 +0000

    v1.5 boundary functions

--- cylinder-membrane-theory-solution.py
+++ cylinder-membrane-theory-solution.py
@@ -95,7 +95,7 @@ M = gl.importLoadMagnitude("M",filename); # (C around y-axis)
 ## cylindrical coordinates r,th,z (radial, circumferential, meridional)
 ### arrays where th = row, z = column
 # th_n = 360;
-z_n = 360;
+z_n = 361;
 # th = np.linspace(0,TH,th_n);
 z = np.linspace(0,H,z_n);
 ### coords[0] = z-coords, coords[1] = th-coords
@@ -149,6 +149,42 @@ N += np.asarray(PStresses(P,R));
 N += np.asarray(TStresses(T,R));
 N += np.asarray(MStresses(M,R));
 N += np.asarray(QStresses(Q,R,H));
+
+### boundary functions
+#### BC1
+f_1 = (-R_x/(2*math.pi*R))-N[1];
+N[1] += f_1;
+
+# #### BC2
+f_2 = (-R_z/(2*math.pi*R)) - N[2];
+N[2] += f_2;
+
+# sanity check
+internalF = N*2*math.pi*R;
+err = [];
+for i,name in enumerate(titles):
+    if i >= 2:
+        break
+    err.append(internalF[2-i]/diagrams[name][0]);
+    
+
+### plot Ns
+# if decision == "Y":
+#     nrows = 2;
+#     ncols = 1;
+    
+#     ylabel = "[N/mm]";
+    
+#     titles = ["N_th: Circumferential Membrane Stress Resultant",
+#               "N_zth: Shear Membrane Stress Resultant",
+#               "N_z: Meridional Membrane Stress Resultant"];
+    
+#     for i,name in enumerate(titles):
+#         plotter(i+5,ncols,nrows,
+#                 name,z,xlabel,
+#                 N[i]*zshapearray,ylabel);
+    
+    
 
 ## think this is redundant?
 # #### add boundary function f_1
